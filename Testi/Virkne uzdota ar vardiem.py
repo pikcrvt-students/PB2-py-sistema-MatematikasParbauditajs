787528f21d9c535d2b6dab779c0e6185e43d8b90
Sidnijs <103423306+21DPSJurc@users.noreply.github.com>
2022-06-09 01:27:49 +0300

    Add files via upload

---
+++ Testi/Virkne uzdota ar vardiem.py
@@ -0,0 +1,26 @@
+from random import randint
+def pārbaude(skaitlis):
+    for i in range(4):
+        if int(input()) != skaitlis * (i+1):
+            return False
+    return True
+while True:
+    try:
+        skaitlis = randint(3,9)
+        atkartot = ''
+        print('Pēteris iedomājās virkni, kuru veido skaitļa '+str(skaitlis)+' dalāmie augošā secībā.\nUzraksti pirmos četrus šīs virknes locekļus!')
+        atbildes = []
+        if pārbaude(skaitlis):
+            print('Tava piedāvātā virkne ir pareiza!')
+        else:
+            print('Tava piedāvātā virkne nav pareiza!')
+        while atkartot != '1' and atkartot != '0':
+            atkartot = input('Vai vēlies atkārtot pārbaudi? 1 - Jā, 0 - Nē ')
+        if atkartot == '1':
+            continue
+        else:
+            break
+    except:
+        continue
+
+    
